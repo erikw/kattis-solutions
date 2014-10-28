d35557de2abe9b1c1129e113aad1686e52ba39e2
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2014-10-28 19:02:59 +0100

    Changed to a recursive is solution. Need a bit better one however.

--- warmup14/basincity/basincity.py
+++ warmup14/basincity/basincity.py
@@ -1,44 +1,66 @@
 #!/usr/bin/env python
 import sys, itertools, random
 
-def are_neighbours(comb, intersections):
-    for c0 in comb:
-        for c1 in comb:
-            if c0 in intersections[c1]:
-                return True
-    return False
-
-def are_neighbours_fast0(comb, intersections):
-    while comb:
-        c0 = comb.pop()
-        for c1 in comb:
-            if c0 in intersections[c1]:
-                return True
-    return False
-
-def are_neighbours_fast1(comb, intersections):
-    for i,c0 in enumerate(comb):
-        for c1 in comb[i+1:]:
-            if c0 in intersections[c1]:
-                return True
-    return False
 
 k = int(raw_input())
 n = int(raw_input())
 
-intersections = []
-for line in sys.stdin:
+
+def remove_vertex(g, v):
+    removed = (v, g[v])
+    for u in g[v]:
+        g[u].remove(v)
+    del g[v]
+    return removed
+
+def reinsert(g, ch):
+    # ch = removed above
+    g[ch[0]] = ch[1]
+    for u in ch[1]:
+        g[u].add(ch[0])
+
+rec_cnt = 0
+def r0(g):
+    global rec_cnt
+    rec_cnt += 1
+
+    if not g:
+        return 0                # 1
+    u = None                    # v w/ most neigh.
+    for v, n in g.iteritems():
+        if not n:
+            ch = remove_vertex(g, v)
+            tmp = 1 + r0(g)   # 2
+            reinsert(g, ch)
+            return tmp
+
+        if not u or len(n) > len(g[u]):
+            u = v
+
+    # 3
+    ch = []
+    nb = set(g[u])
+    ch.append(remove_vertex(g, u))
+    bval = r0(g)
+    for v in nb:
+        ch.append(remove_vertex(g, v))
+    aval = 1 + r0(g)
+    for c in reversed(ch):
+        reinsert(g, c)
+
+    return max(aval, bval)
+
+
+
+intersections = {}
+for i, line in enumerate(sys.stdin):
     line = line.split()
     neighbours = set(int(x) - 1 for x in line[1:])
-    intersections.append(neighbours)
+    intersections[i] = neighbours
 
-r = range(0, n)
-random.shuffle(r)
-combs = itertools.combinations(r, k)
-#for c in combs:
-    #print c
+mis = r0(intersections)
 
-if all(are_neighbours_fast1(c, intersections) for c in combs):
+if mis < k:
     print "impossible"
 else:
     print "possible"
