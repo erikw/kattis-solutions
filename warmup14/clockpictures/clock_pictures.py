ca5c56ee22f611c6ac669cb4503bf29c81076c11
Erik Jansson <erikjansson90@gmail.com>
2014-12-09 17:54:03 +0100

    Cleanup clockpictures.
    Apparently, Rabin-Karp is inferior to KMP for single pattern searching.

--- warmup14/clockpictures/clock_pictures.py
+++ warmup14/clockpictures/clock_pictures.py
@@ -1,47 +1,5 @@
 #!/usr/bin/env python
 
-def sublistExists(list1, list2):
-    return ' '.join(map(str, list2)) in ' '.join(map(str, list1))
-
-
-def naive_sublist_search(l0, l1):
-    for i in range(0, len(l0) - len(l1) + 1):
-        in_pattern = True
-        for j in range(0, len(l1)):
-            if l0[i + j - 1] != l1[j]:
-                in_pattern = False
-                break
-        if not in_pattern:
-            continue
-        return True # Can return index here instead.
-    return False
-
-
-def hash_func(seq):
-    p = 11
-    power = 1
-    h = 0
-    for i in range(0, len(seq)):
-        h += power * seq[len(seq) - i - 1]
-        power *= p
-    return h
-
-def rabin_karp_sublist_search(s, pattern):
-    m = len(pattern)
-    n = len(s)
-    hs = hash_func(s[:m])
-    hp = hash_func(pattern)
-
-    print (11**(m - 1))
-    for i in range(0, n - m):
-        #print hs, hp, s[i:i + m], pattern
-        if hs == hp:
-            if s[i:i + m] == pattern:
-                return True # Can return index here instead.
-        hs = 11 * (hs - s[i]* 11**(m - 1)) + s[i + m]
-    return False
-
-
 def kmp_table(word):
     """Compute the Knuth-Morris-Pratt substring search table.
 
@@ -72,9 +30,6 @@ def kmp_search(text, word):
     m = 0
     i = 0
     t = kmp_table(word)
-    print t
-    print text
-    print word
     while m + i < len(text):
         if word[i] == text[m + i]:
             if i == len(word) - 1:
