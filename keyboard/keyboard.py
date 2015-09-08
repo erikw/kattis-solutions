2e47a8098e703a0d8caf52f37a4192a310fd1060
Erik Jansson <erikjansson90@gmail.com>
2015-09-08 20:04:38 +0200

    Read input for keyboard.

--- keyboard/keyboard.py
+++ keyboard/keyboard.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python 
 
 
+#import pprint
+
+def read_input():
+    r, c = [int(d) for d in raw_input().split()]
+    keyboard = {} # char -> [(x,y)]
+    for y in range(r):
+        for x, unit in enumerate(raw_input().strip()):
+            if unit not in keyboard:
+                keyboard[unit] = []
+            keyboard[unit].append((x,y))
+    string = raw_input().split()
+    return r, c, string
+
+
 def main():
-    pass
+    r, c, string = read_input()
 
 if __name__ == '__main__':
     main()
