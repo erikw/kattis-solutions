c6d54cf0a24581ef71a3d816ca5b0464cac0ed4e
Erik Jansson <erikjansson90@gmail.com>
2015-11-10 17:56:37 +0100

    Update most awesomest setup script!!!!111!1oneoneeleven
    Now with the following amaZing features:
    
    	* chmod u+x
    	* Download sample data!!!
    	* UNZIP SAMPLE DATA
    	* REMOVE DOWNLOADED ZIP FILE
    	* NOT-AT-ALL-HACKY RENAME FUNCTION

--- setup.sh
+++ setup.sh
@@ -10,3 +10,18 @@ def main():
 if __name__ == '__main__':
     main()
 ENDOFSTRING
+
+chmod u+x "$1/$1.py"
+
+# Fetch sample files.
+wget --quiet -O "$1/samples.zip" "https://open.kattis.com/problems/$1/file/statement/samples.zip"
+unzip -q "$1/samples.zip" -d "$1"
+rm "$1/samples.zip"
+
+# Hack to rename files if there is only one sample input.
+if [ -f "$1/sample.in" ]; then
+	mv "$1/sample.in" "$1/0.in"
+fi
+if [ -f "$1/sample.ans" ]; then
+	mv "$1/sample.ans" "$1/0.ans"
+fi
